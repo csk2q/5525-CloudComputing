594e87c1e8c1a8207d197608695bcaf18af67e9b
csk2q <143836825+csk2q@users.noreply.github.com>
2025-06-30 19:58:47 -0500

    Make the startup script its own file

--- llmProject/main.tf
+++ llmProject/main.tf
@@ -91,31 +91,7 @@ resource "google_compute_instance" "llm_vm" {
 
   # To see the output of this script run the following:
   # sudo journalctl -u google-startup-scripts.service
-  metadata_startup_script = <<-EOT
-    #!/bin/bash
-    echo hi > /test.txt
-
-    # Add Docker's official GPG key:
-    sudo apt-get update
-    sudo apt-get install -y ca-certificates curl
-    sudo apt-get install -y apt-transport-https gnupg
-
-    sudo install -m 0755 -d /etc/apt/keyrings
-    sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-    sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-    # Add the repository to Apt sources:
-    echo \
-      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
-      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-    sudo apt-get update
-    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-    systemctl enable docker
-    systemctl start docker
-  EOT
+  metadata_startup_script = file("${path.module}/startup.sh")
 
   tags = ["http-server", "https-server"]
 }
